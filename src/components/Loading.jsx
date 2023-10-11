1f7bd6521f0b927e3f5b64d0561c928df2bea315
Jeffrey <jdsun010@gmail.com>
2023-10-11 10:39:47 -0700

    add Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,16 @@
+const Loading = () => {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '10rem 0'
+        }}
+      >
+        <div className='loading' />
+      </div>
+    );
+}
+
+export default Loading;
